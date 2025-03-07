24d7cba4da18254887caa494fb68ac76a9f81bf4
bilalaltinn <bilalaltn34@gmail.com>
2025-03-07 13:54:53 +0300

    Version 1.1

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float xValue = 0.00f;
+    [SerializeField] float yValue = 0.01f;
+    [SerializeField] float zValue = 0.00f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,6 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(xValue, yValue, zValue);
     }
 }
